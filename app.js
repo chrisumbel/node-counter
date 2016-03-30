593b87bcb9beebb5a09edb1b0f3309fdae1e5f14
Chris Umbel <chris@chrisumbel.com>
2016-03-30 12:16:25 -0500

    added instance index to output

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require("express");
 var redis = require('redis');
 
 var creds = null;
+var j = 0;
 
 console.log('starting');
 
@@ -12,11 +13,14 @@ console.log(creds);
 var client = redis.createClient(creds.port, creds.host, {auth_pass: creds.password});
 
 app.get("/", function(req, res) { 
-	console.log('viewing count ' + i);
-
 	var i = client.incr('viewCount', function() {
-		client.get('viewCount', function(err, i) {
-			res.end(i.toString());
+		client.get('viewCount', function(err, j) {
+			res.contentType("text/html");
+			res.end('<h1>Instance: ' + 
+				process.env.CF_INSTANCE_INDEX + 
+				'</br> Count: ' + 
+				j.toString() + 
+				'</h1>');
 		    });
 	    });
     });
